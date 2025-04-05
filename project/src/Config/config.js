74302bdcc20bf8f8a644870d012255e80818b501
Honeysingh0632 <honeysing00632@gmail.com>
2025-04-05 18:14:32 +0530

    Update config.js

--- project/src/Config/config.js
+++ project/src/Config/config.js
@@ -1,3 +1,3 @@
- export const baseurl = `http://localhost:8000`
+ //export const baseurl = `http://localhost:8000`
  export const keyid= `rzp_test_IBv200U5UqHLvw`
- //export const baseurl = `https://first-project-backend-ivgp.onrender.com`
+ export const baseurl = `https://library-managment-sgag.onrender.com`
